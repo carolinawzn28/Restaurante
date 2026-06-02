48b520014ae3a8ade15d70608ff34aaa30445406
Carolina Denisse Soto Barbosa <carolinasoto3h@gmail.com>
2026-06-01 21:53:31 -0700

    Update Mesero.java

--- Mesero.java
+++ Mesero.java
@@ -4,12 +4,11 @@ class Mesero extends Empleado {
     public double propinasDia;
     public int meserosTurno;
 
-    public Mesero(int id, String nombre, int salario, double propinasDia, int meserosTurno) {
-        super(id, nombre, salario, meserosTurno); 
+    public Mesero(int id, String nombre, String tel, double salario, double propinasDia, int meserosTurno) {
+        super(id, nombre, tel, salario);
         this.propinasDia = propinasDia;
         this.meserosTurno = meserosTurno;
     }
-
 
     @Override
     public double calcularSueldo() {
