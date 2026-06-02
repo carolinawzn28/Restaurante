72f5335cdb157a1a8ed0c842003406021d4b2f79
Carolina Denisse Soto Barbosa <carolinasoto3h@gmail.com>
2026-06-01 21:54:21 -0700

    Update RecetaCarne.java

--- RecetaCarne.java
+++ RecetaCarne.java
@@ -1,9 +1,6 @@
 package jdbc_ejemplo1;
 
-import javax.swing.JOptionPane;
-
 public class RecetaCarne extends ABC_Recetas {
-    
     public String lote;
     public String terminoCoccion;
 
@@ -14,12 +11,7 @@ public class RecetaCarne extends ABC_Recetas {
         this.terminoCoccion = termino;
     }
 
-   
-    public void verRecetaCarne() {
-        String mensaje = "RECETA DE CARNE\n" +
-                         "Nombre: " + this.txtNombre.getText() + "\n" +
-                         "Lote: " + this.lote + "\n" +
-                         "Término: " + this.terminoCoccion;
-        JOptionPane.showMessageDialog(null, mensaje);
+    public void verReceta() {
+        super.verDetalleReceta("Tipo: Carne\nLote: " + this.lote + "\nTérmino Cocción: " + this.terminoCoccion);
     }
 }
