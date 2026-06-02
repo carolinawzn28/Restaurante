203af603a1361763ecf96b21ad479ee143a04787
Carolina Denisse Soto Barbosa <carolinasoto3h@gmail.com>
2026-06-01 21:52:53 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ package jdbc_ejemplo1;
 public class Main {
     public static void main(String[] args) {
         Login l = new Login();
-        l.setVisible(true);  
+        l.setVisible(true);
     }
 }
